afa9d73f66be98ac2ba13d3a9600b58194f0961f
PrathamDwivedi27 <pratham.gungun12345@gmail.com>
2024-06-16 18:35:24 +0530

    Completed Frontend part

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     <>
       <div>
         <dialog id="my_modal_3" className="modal">
-          <div className="modal-box">
+          <div className="modal-box dark:bg-slate-900 dark:text-white">
             <form onSubmit={handleSubmit(onSubmit)} method="dialog">
               {/* if there is a button in form, it will close the modal */}
               <Link
