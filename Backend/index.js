357dd5544bbff26483ff92e1349e837d28f4a1dd
Sajal Namdeo <113107248+Sajal072004@users.noreply.github.com>
2024-06-17 10:50:02 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.use(cors());
+
 
 const PORT=process.env.PORT || 4000;
 const URI=process.env.MongoDBURI;
