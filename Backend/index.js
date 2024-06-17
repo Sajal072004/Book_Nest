11fa546ca137d2aa6aa8a0eaa57f879cae7482f6
Sajal Namdeo <113107248+Sajal072004@users.noreply.github.com>
2024-06-17 10:54:21 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,10 +11,14 @@ const app=express();
 const corsOptions = {
     origin: "https://book-nest-wine.vercel.app",
     methods: ["POST", "GET", "OPTIONS"],
-    credentials: true
+    credentials: true,
+    optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); // Apply CORS configuration
+
+// Handle preflight requests
+app.options('*', cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
