1b8735988cf36702962ebb194762b72fa440f608
Sajal Namdeo <113107248+Sajal072004@users.noreply.github.com>
2024-06-17 10:28:28 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,13 @@ import cors from 'cors'
 dotenv.config();
 const app=express();
 
+app.use(cors(
+    {
+        origin: [""],
+        methods: ["POST"  , "GET"],
+        credentials: true,
+    }
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
